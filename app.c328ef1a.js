b1a15caf7c90a3eb3d7a2aff5256f010c601c403
Marko Ronik <13383578+ronca85@users.noreply.github.com>
2020-02-29 17:56:04 +0100

    edits color

--- app.c328ef1a.js
+++ app.c328ef1a.js
@@ -45134,7 +45134,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56971" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57077" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
